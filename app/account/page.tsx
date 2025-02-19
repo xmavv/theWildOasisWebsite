b6c1dd4f8741c39bc1411f09f14062020b2a210a
xmavv <xxmatheooo@gmail.com>
2025-02-19 23:48:41 +0100

    creating new guest on first signin and mutating the session object with user id from database

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -6,6 +6,8 @@ export default async function Page() {
   const session = await auth();
   const firstname = session?.user.name.split(" ").at(0);
 
+  console.log(session);
+
   return (
     <h2 className="font-semibold text-2xl text-accent-400 mb-7">
       Welcome, {firstname}
